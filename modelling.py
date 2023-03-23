b989e55f7c5a4ac7210dc49e384acf0dd8f711b7
Nico Marshall <nico.marshall@btinternet.com>
2023-03-23 16:23:57 +0000

    removed nan columns. Linear Model compiles

--- modelling.py
+++ modelling.py
@@ -5,8 +5,10 @@ from tabular_data import load_airbnb
 
 
 model = linear_model.SGDRegressor()
+#model = linear_model.LinearRegression()
 
-
-if __name__ == "main":
-    load_airbnb("clean_tabular_data.csv","price_night")
-    
+if __name__ == "__main__":
+    features, labels = load_airbnb("clean_tabular_data.csv","Price_Night")
+    model.fit(features, labels)
+    print(model.predict(features)[0:10])
+    print(labels[0:10])
