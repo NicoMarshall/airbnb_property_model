02fe5b03e578f49667dee01ba669ff1a1d129d78
Nico Marshall <nico.marshall@btinternet.com>
2023-04-14 15:31:58 +0100

    hyper param grid search

--- modelling.py
+++ modelling.py
@@ -40,19 +40,7 @@ if __name__ == "__main__":
     hyperparameter_dict = {"loss":["squared_error", "huber", "epsilon_insensitive", "squared_epsilon_insensitive"],"penalty":["l2", "l1", "elasticnet",None],"alpha":[0.0001,0.0005,0.001],"shuffle":[True,False],"learning_rate":["constant","optimal","invscaling"]}
     optimals = custom_tune_regression_hyperparameters(linear_model.SGDRegressor,x_train ,x_test ,y_train ,y_test,x_validation,y_validation,hyperparameter_dict)
     print(optimals)
-    """
-    model.fit(x_train, y_train)
-    y_pred_test = model.predict(x_test)
-    y_pred_train = model.predict(x_train)
-    rmse_skl_train = metrics.mean_squared_error(y_pred_train, y_train, squared = False)
-    rmse_skl_test = metrics.mean_squared_error(y_pred_test, y_test, squared = False)
-    print("test loss",rmse_skl_test)
-    print("training loss",rmse_skl_train)
-    r_2_training = metrics.r2_score(y_train, y_pred_train)
-    r_2_test = metrics.r2_score(y_test, y_pred_test)
-    print("r_2_training",r_2_training)
-    print("r_2_test",r_2_test)
-    """     
+   
 
 
     
