0769e1e7b6e1fc7afc5cf6e71d8332650770a60e
Nico Marshall <nico.marshall@btinternet.com>
2023-04-14 15:30:50 +0100

    hyperparam grid search implemented

--- modelling.py
+++ modelling.py
@@ -2,14 +2,45 @@ import numpy as np
 import joblib
 from sklearn import datasets, model_selection, linear_model,metrics
 from tabular_data import load_airbnb
+import itertools
 
 
-#model = linear_model.SGDRegressor() #
-model = linear_model.LinearRegression()
+def makeGrid(my_dict):  
+    keys=my_dict.keys()
+    combinations=itertools.product(*my_dict.values())
+    ds=[dict(zip(keys,cc)) for cc in combinations]
+    return ds
 
+def custom_tune_regression_hyperparameters(model_class, x_train, x_test, y_train,y_test, x_validation,y_validation, hyperparameter_dict):
+    param_combinations = makeGrid(hyperparameter_dict)
+    best_params = None
+    best_rmse = 1000000000
+    for i in param_combinations:
+        model=  model_class(loss =i["loss"],penalty=i["penalty"],alpha=i["alpha"],learning_rate=i["learning_rate"])
+        model.fit(x_train,y_train)
+        y_pred = model.predict(x_validation)
+        rmse_validation = metrics.mean_squared_error(y_pred,y_validation,squared = False)
+        r_2_validation = metrics.r2_score(y_pred,y_validation)
+        if rmse_validation < best_rmse:
+            best_rmse = rmse_validation
+            best_params = i
+            r_2_score = r_2_validation
+        else:
+            pass
+    hyperparameter_optimals = {"parameters": best_params,"rmse": best_rmse,"r_2": r_2_score}
+    
+    return hyperparameter_optimals
+   
+    
+        
 if __name__ == "__main__":
     features, labels = load_airbnb("clean_tabular_data.csv","Price_Night")
     x_train ,x_test ,y_train ,y_test = model_selection.train_test_split(features,labels, test_size=0.3)
+    x_validation, x_test, y_validation,y_test = model_selection.train_test_split(x_test,y_test,test_size = 0.5)
+    hyperparameter_dict = {"loss":["squared_error", "huber", "epsilon_insensitive", "squared_epsilon_insensitive"],"penalty":["l2", "l1", "elasticnet",None],"alpha":[0.0001,0.0005,0.001],"shuffle":[True,False],"learning_rate":["constant","optimal","invscaling"]}
+    optimals = custom_tune_regression_hyperparameters(linear_model.SGDRegressor,x_train ,x_test ,y_train ,y_test,x_validation,y_validation,hyperparameter_dict)
+    print(optimals)
+    """
     model.fit(x_train, y_train)
     y_pred_test = model.predict(x_test)
     y_pred_train = model.predict(x_train)
@@ -21,7 +52,7 @@ if __name__ == "__main__":
     r_2_test = metrics.r2_score(y_test, y_pred_test)
     print("r_2_training",r_2_training)
     print("r_2_test",r_2_test)
-
+    """     
 
 
     
