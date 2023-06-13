0439fdf26b5f9300eff52627d987f28bd3f967bf
Nico Marshall <nico.marshall@btinternet.com>
2023-06-13 14:39:31 +0100

    tboard GUI works now

--- pytorch_model.py
+++ pytorch_model.py
@@ -5,14 +5,25 @@ import pandas as pd
 import numpy as np
 from tabular_data import load_airbnb
 from sklearn.model_selection import train_test_split
+from torch.utils.tensorboard import SummaryWriter
 
 class nn_model(nn.Module):
     def __init__(self):
         super().__init__()
-        self.layer = nn.Linear(11,1)
+        self.layer1 = nn.Linear(11,30)
+        self.layer2 = nn.BatchNorm1d(30)
+        self.layer3 = nn.Linear(30,30)
+        self.layer4 = nn.BatchNorm1d(30)
+        self.layer5 = nn.Linear(30,1)
         
-    def forward(self, features):    
-        return self.layer(features)
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x =  F.relu(self.layer2(x)) 
+        x =  F.relu(self.layer3(x))
+        x =  F.relu(self.layer4(x))
+        x =  F.relu(self.layer5(x))
+        
+        return x
     
     
 class AirbnbNightlyPriceRegressionDataset(torch.utils.data.Dataset):
@@ -27,15 +38,30 @@ class AirbnbNightlyPriceRegressionDataset(torch.utils.data.Dataset):
         return len(self.labels)
  
  
-def train(model_class, dataloader, num_epochs = 5): 
+def train(model_class, train_dataloader, val_dataloader, learn_rate = 0.1 , num_epochs = 5 ): 
     model = model_class()
+    writer = SummaryWriter()
+    optimiser = torch.optim.SGD(model.parameters(), lr = learn_rate)
+    torch.manual_seed(0)
     for i in range(num_epochs):
-        for batch in train_dataloader:
-            features, labels = batch
-            print(model(features))
-            break
-        break
-    
+        for features, labels in train_dataloader:
+            pred = model(features).view([len(labels)])
+            loss = torch.sqrt(F.mse_loss(pred, labels))
+            optimiser.zero_grad()
+            loss.backward()
+            optimiser.step()
+            writer.add_scalar("Training Loss", loss, i)
+        #if i%5 == 0:    
+            with torch.no_grad():
+                val_loss = 0
+                for features, labels in val_dataloader:
+                        pred = model(features).view([len(labels)])
+                        val_loss = torch.sqrt(F.mse_loss(pred, labels))
+                #print(f"Loss in epoch#{i} was {val_loss}")
+                writer.add_scalar("Validation Loss", val_loss, i)
+        #else:
+            #pass          
+    return model
     
 if __name__ == "__main__":
     features, labels = load_airbnb("clean_tabular_data.csv","Price_Night")
@@ -45,5 +71,6 @@ if __name__ == "__main__":
     test_data = AirbnbNightlyPriceRegressionDataset(x_test, y_test)
     validation_data = AirbnbNightlyPriceRegressionDataset(x_validation, y_validation)
     train_dataloader = torch.utils.data.DataLoader(train_data, batch_size = 32, shuffle = True)
-    train (nn_model, train_dataloader)
+    validation_dataloader = torch.utils.data.DataLoader(validation_data, batch_size = len(validation_data), shuffle = True)
+    train (nn_model, train_dataloader, validation_dataloader, num_epochs= 31, learn_rate=0.1)
               
