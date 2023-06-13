1b3dc7a61af9c7b97e1aca0dd99ff5456cc817df
Nico Marshall <nico.marshall@btinternet.com>
2023-06-13 10:21:38 +0100

    nn_model initialised

--- pytorch_model.py
+++ pytorch_model.py
@@ -6,7 +6,15 @@ import numpy as np
 from tabular_data import load_airbnb
 from sklearn.model_selection import train_test_split
 
-
+class nn_model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer = nn.Linear(11,1)
+        
+    def forward(self, features):    
+        return self.layer(features)
+    
+    
 class AirbnbNightlyPriceRegressionDataset(torch.utils.data.Dataset):
     def __init__(self, x, y):
         super().__init__()
@@ -17,6 +25,17 @@ class AirbnbNightlyPriceRegressionDataset(torch.utils.data.Dataset):
     
     def __len__(self):
         return len(self.labels)
+ 
+ 
+def train(model_class, dataloader, num_epochs = 5): 
+    model = model_class()
+    for i in range(num_epochs):
+        for batch in train_dataloader:
+            features, labels = batch
+            print(model(features))
+            break
+        break
+    
     
 if __name__ == "__main__":
     features, labels = load_airbnb("clean_tabular_data.csv","Price_Night")
@@ -25,7 +44,7 @@ if __name__ == "__main__":
     train_data = AirbnbNightlyPriceRegressionDataset(x_train, y_train)
     test_data = AirbnbNightlyPriceRegressionDataset(x_test, y_test)
     validation_data = AirbnbNightlyPriceRegressionDataset(x_validation, y_validation)
-    print(x_train.shape, y_train.shape, x_validation.shape, y_validation.shape)
+    #print(x_train.shape, y_train.shape, x_validation.shape, y_validation.shape)
     train_dataloader = torch.utils.data.DataLoader(train_data, batch_size = 32, shuffle = True)
-   
+    train (nn_model, train_dataloader)
               
