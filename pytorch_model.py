7ab50f2b8677a146a8ec39b4bf72a1620d675dc7
Nico Marshall <nico.marshall@btinternet.com>
2023-06-20 16:13:48 +0100

    model eval and save methods added

--- pytorch_model.py
+++ pytorch_model.py
@@ -1,12 +1,19 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F 
+import torcheval
+from torcheval.metrics import MeanSquaredError,  R2Score
+from torcheval.metrics.functional import r2_score
 import pandas as pd
 import numpy as np
 from tabular_data import load_airbnb
 from sklearn.model_selection import train_test_split
 from torch.utils.tensorboard import SummaryWriter
 import yaml
+import os
+import json
+import datetime
+import time
 
 class nn_model(nn.Module):
     def __init__(self):
@@ -75,7 +82,7 @@ class nn_config(nn.Module):
             activation = layer_config["activation"]
             if type == "dense":
                 layers.append(nn.Linear(input_size, width))
-            if activation == "relu":
+            elif activation == "relu":
                 layers.append(nn.ReLU())
             elif activation == "softmax":
                 layers.append(nn.softmax)(dim =1)
@@ -92,6 +99,7 @@ def get_nn_config(config_file: yaml):
     return config
 
 def config_train(model_class, config, train_dataloader, val_dataloader):
+    start_time = time.perf_counter()
     model = model_class(config)
     optimiser = getattr(torch.optim, config["optimiser"]["name"])
     learning_rate = config["optimiser"]["learning_rate"]
@@ -106,6 +114,7 @@ def config_train(model_class, config, train_dataloader, val_dataloader):
             train_loss.backward()
             optimiser.step()
             #writer.add_scalar("Training Loss", loss, i)
+        """
         if i%5 == 0:    
             with torch.no_grad():
                 val_loss = 0
@@ -115,20 +124,73 @@ def config_train(model_class, config, train_dataloader, val_dataloader):
                 print(f"Loss in epoch#{i} was {val_loss}")
                 #writer.add_scalar("Validation Loss", val_loss, i)
         else:
-            pass          
-    return model
+            pass
+        """ 
+    train_time = time.perf_counter() - start_time         
+    return model, train_time
     
-    
+def eval_model(model,train_dataloader,validation_dataloader,test_dataloader,train_size):
+    with torch.no_grad():
+        loss = MeanSquaredError()
+        r2_score = R2Score()
+        inference_latency = 0
+        for features, labels in train_dataloader:
+            start_time = time.perf_counter()
+            pred = model(features).view(len(labels))
+            end_time = time.perf_counter()
+            inference_latency += end_time-start_time
+            loss.update(pred, labels)
+            r2_score.update(pred, labels)
+        train_loss = torch.sqrt(loss.compute())
+        train_r2_score = r2_score.compute()
+        inference_latency /= train_size
+        for features, labels in test_dataloader:
+            pred = model(features).view(len(labels))
+            test_loss = torch.sqrt(F.mse_loss(pred, labels))
+            test_r2_score = torcheval.metrics.functional.r2_score(pred, labels)
+        for features, labels in validation_dataloader:
+            pred = model(features).view(len(labels))
+            validation_loss = torch.sqrt(F.mse_loss(pred, labels))
+            validation_r2_score = torcheval.metrics.functional.r2_score(pred, labels)
+        metrics = [train_loss, test_loss, validation_loss, train_r2_score, test_r2_score, validation_r2_score, inference_latency]
+        metrics = np.array(metrics)
+        metric_labels = ["train_loss", "test_loss", "validation_loss", "train_r2_score", "test_r2_score", "validation_r2_score", "inference_latency"]
+        metric_dict = dict(zip(metric_labels,metrics))
+        return metric_dict
+
+def save_model(folder,model,config,metrics):
+    working_dir = os.getcwd()
+    if isinstance(model, nn.Module):
+        date_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        os.chdir(folder)
+        os.mkdir(date_time)
+        os.chdir(date_time)
+        torch.save(model,"model.pt")
+        with open("hyperparameters.json", "w") as outfile:
+            json.dump(config, outfile)
+        with open("metrics.json", "w") as outfile:
+            json.dump(metrics, outfile)
+        os.chdir(working_dir)
+    else:
+        print("Not a Pytorch model")
+        
+      
     
 if __name__ == "__main__":
     features, labels = load_airbnb("clean_tabular_data.csv","Price_Night")
     x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size= 0.2)
     x_validation, x_test, y_validation, y_test = train_test_split(x_test, y_test, test_size= 0.5)
     train_data = AirbnbNightlyPriceRegressionDataset(x_train, y_train)
+    train_size = len(train_data)
     test_data = AirbnbNightlyPriceRegressionDataset(x_test, y_test)
     validation_data = AirbnbNightlyPriceRegressionDataset(x_validation, y_validation)
     train_dataloader = torch.utils.data.DataLoader(train_data, batch_size = 32, shuffle = True)
     validation_dataloader = torch.utils.data.DataLoader(validation_data, batch_size = len(validation_data), shuffle = True)
+    test_dataloader = torch.utils.data.DataLoader(test_data, batch_size = len(test_data), shuffle = True)
     #train (nn_model, train_dataloader, validation_dataloader, num_epochs= 31, learn_rate=0.1)
     config = get_nn_config("nn_config.yaml")
-    config_train(nn_config, config, train_dataloader, validation_dataloader)        
+    model, train_time = config_train(nn_config, config, train_dataloader, validation_dataloader)        
+    metrics = eval_model(model, train_dataloader, validation_dataloader, test_dataloader, train_size)
+    metrics["train time"] = train_time
+    dest_folder="C:\\Users\\nicom\\OneDrive\\Υπολογιστής\\airbnb_property_model\\models\\neural_networks\\regression"
+    save_model(dest_folder,model,config,metrics)
