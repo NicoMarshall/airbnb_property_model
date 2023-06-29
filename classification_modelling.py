e3cc70bcaab9054ee4c06edfdc70a4f4dae4e690
Nico Marshall <nico.marshall@btinternet.com>
2023-06-29 21:57:49 +0100

    minor changes

--- classification_modelling.py
+++ classification_modelling.py
@@ -43,27 +43,31 @@ def save_model(folder,model,best_params,best_metrics):
 def tune_hyperparameters(model_class, features, labels, hyperparam_dict):
     hyperparam_combs = makeGrid(hyperparam_dict)
     best_hyperparams = None
-    best_validation_accuracy = 0
+    best_test_accuracy = 0
     best_model = None
     best_model_metrics = None
+    x_train ,x_test ,y_train ,y_test = model_selection.train_test_split(features,labels, test_size=0.3)
+    x_validation, x_test, y_validation,y_test = model_selection.train_test_split(x_test,y_test,test_size = 0.5)
     for i in hyperparam_combs:
         try:
-            x_train ,x_test ,y_train ,y_test = model_selection.train_test_split(features,labels, test_size=0.3)
-            x_validation, x_test, y_validation,y_test = model_selection.train_test_split(x_test,y_test,test_size = 0.5)
             model = model_class(**i)
             model.fit(x_train,y_train)
-            metrics = evaluate_model(model, x_validation,y_validation, label_list)
-            if metrics[0] > best_validation_accuracy:
-                best_validation_accuracy = metrics[0]
+            mod_metrics = evaluate_model(model, x_test, y_test, label_list)
+            if mod_metrics[0] > best_test_accuracy:
+                best_test_accuracy = mod_metrics[0]
                 best_hyperparams = i
                 best_model = model
-                best_model_metrics = metrics
+                best_model_metrics = mod_metrics
             else:
                 pass
         except:
             ValueError
             pass
-        
+    y_pred = best_model.predict(x_test)
+    confusion_matrix = metrics.confusion_matrix(y_pred, y_test, labels = label_list)
+    disp = metrics.ConfusionMatrixDisplay(confusion_matrix, display_labels=label_list)
+    disp.plot()
+    plt.show()
     return best_model, best_hyperparams, best_model_metrics 
  
 def find_best_model(task_folder:str):
@@ -93,6 +97,7 @@ def find_best_model(task_folder:str):
 if __name__ =="__main__":
     features, labels = load_airbnb("clean_tabular_data.csv","Category")
     label_list = list(set(labels))
+    
     hyperparam_dict_logistic = {"penalty":["l1", "l2", "elasticnet", None], "dual": [True, False], "C" : [0.5,1,1.5,2], "solver":["liblinear", "newton-cg", "newton-cholesky", "sag", "saga"], "max_iter": [500]}
     hyperparam_dict_random_forest = {"n_estimators":[50,100,150,200],"criterion":["gini", "entropy", "log_loss"],"max_depth":[None,10,20],"max_features":["sqrt", "log2", None]}
     hyperparam_dict_grad_boost = {"loss":["log_loss","deviance","exponential"],"learning_rate":[0.001,0.01,0.1],"n_estimators":[100,150,200]}
@@ -101,6 +106,22 @@ if __name__ =="__main__":
     file_dest = f"C:\\Users\\nicom\\OneDrive\\Υπολογιστής\\airbnb_property_model\\models\\classification\\random_forest"
     save_model(file_dest, model, best_hyperparams, best_model_metrics )
     task_folder = f"C:\\Users\\nicom\\OneDrive\\Υπολογιστής\\airbnb_property_model\\models\\classification"
-    find_best_model(task_folder)
     
     
+    """"
+    x_train ,x_test ,y_train ,y_test = model_selection.train_test_split(features,labels, test_size=0.3)
+    x_validation, x_test, y_validation,y_test = model_selection.train_test_split(x_test,y_test,test_size = 0.5)
+    model_path = f"C:\\Users\\nicom\\OneDrive\\Υπολογιστής\\airbnb_property_model\\models\\classification\\random_forest\\model.joblib"
+    model_1 = joblib.load(model_path)
+    
+    acc = evaluate_model(model_1, x_test, y_test, label_list)[0]
+    print(acc)
+    
+    y_pred = model_1.predict(x_test)
+    
+    accuracy_score_1 = metrics.accuracy_score(y_test,y_pred)
+    
+    print(accuracy_score_1)
+    
+"""
+   
