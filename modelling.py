da87f48729c8cd1bcf3b9f70e04d4b7a68cfac7a
Nico Marshall <nico.marshall@btinternet.com>
2023-07-18 18:04:37 +0100

    docstrings

--- modelling.py
+++ modelling.py
@@ -1,25 +1,24 @@
-import numpy as np
+import itertools
+import json
 import joblib
+import numpy as np
+import warnings
 from sklearn import datasets, model_selection, linear_model,metrics, ensemble
 from sklearn.model_selection import GridSearchCV
-import warnings
 from sklearn.exceptions import ConvergenceWarning
-
-warnings.filterwarnings("ignore", category=ConvergenceWarning)
 from tabular_data import load_airbnb
-import itertools
-import os
-import json
-import warnings
 
 
 def makeGrid(my_dict):  
+    """Generate a grid of combinations from a dictionary where all values are iterables eg lists"""
     keys=my_dict.keys()
     combinations=itertools.product(*my_dict.values())
     ds=[dict(zip(keys,cc)) for cc in combinations]
     return ds
 
 def custom_tune_regression_hyperparameters(model_class, x_train, x_test, y_train,y_test, x_validation,y_validation, hyperparameter_dict):
+    """custom grid search implementation. Not used in main code block,superceded by tune_regression_model_hyperparaemeters
+    """
     param_combinations = makeGrid(hyperparameter_dict)
     best_params = None
     best_rmse = 1000000000
@@ -44,22 +43,42 @@ def custom_tune_regression_hyperparameters(model_class, x_train, x_test, y_train
     return hyperparameter_optimals
  
 def tune_regression_model_hyperparameters(features,labels, model_class, hyperparameter_dict):
+    """Uses sklearn's GridSearchCV to do a hyperparameter grid search
+
+    Args:
+        features (pd.Dataframe): features data
+        labels (pd.Series): labels data
+        model_class (class): sklearn regression class
+        hyperparameter_dict (dict): dictionary of hyperparameters to try
+
+    Returns:
+        best_model and associated metrics ad hyperparameters
+    """
     model = model_class()
     grid_search = GridSearchCV(model,hyperparameter_dict,scoring = "neg_root_mean_squared_error")
+    warnings.filterwarnings("ignore", category=ConvergenceWarning)
     grid_search.fit(features,labels)
     best_params = grid_search.get_params()
     best_params["estimator"] = str(best_params["estimator"])
     best_loss = np.abs(grid_search.best_score_)
     return grid_search,best_params,best_loss
 
-def model_search(model_list: list, hyper_dicts: list):
-    #for model_class, model_name, hyperparams in zip(model_list, model_names, hyper_dicts):
-    model_class, model_name, hyperparams = ensemble.RandomForestRegressor, "Random Forest", hyperparam_dict_random_forest
-    model, best_params, best_loss = tune_regression_model_hyperparameters(features, labels, model_class=model_class, hyperparameter_dict=hyperparams)
-    save_model(folder=f"{root}\\models\\regression\\{model_name}", model=model, best_params=best_params, best_loss=best_loss)
+def model_search(model_list: list, model_names:list, hyper_dicts: list):
+    """Loops through model classes and performs hyperparameter grid search on each
+
+    Args:
+        model_list (list): list of model classes to try
+        hyper_dicts (list): dictionary of hyperparameters to try for each model class
+        model_names (list): list of model names as strings, which will be the folder names where they'll be saved
+    """
+    for model_class, model_name, hyperparams in zip(model_list, model_names, hyper_dicts):
+        model, best_params, best_loss = tune_regression_model_hyperparameters(features, labels, model_class=model_class, hyperparameter_dict=hyperparams)
+        save_model(folder=f"{root}\\models\\regression\\{model_name}", model=model, best_params=best_params, best_loss=best_loss)
     
 
 def save_model(folder,model,best_params,best_loss):
+    """Saves model and associated hyperparameters/metrics in file specified by folder.
+    """
     try:
         os.mkdir(folder)
     except FileExistsError:
@@ -77,8 +96,6 @@ def save_model(folder,model,best_params,best_loss):
 if __name__ == "__main__":
     root = os.getcwd()
     features, labels = load_airbnb("clean_tabular_data.csv","Price_Night")
-    #x_train, x_test , y_train , y_test = model_selection.train_test_split(features,labels, test_size=0.3)
-   # x_validation, x_test, y_validation, y_test = model_selection.train_test_split(x_test,y_test,test_size = 0.5)
     hyperparameter_dict_linear = {"loss":["squared_error", "huber", "epsilon_insensitive", "squared_epsilon_insensitive"],"penalty":["l2", "l1", "elasticnet",None],"alpha":[0.0001,0.0005,0.001,0.1,0.001],"shuffle":[True,False],"learning_rate":["constant","optimal","invscaling"]}
     hyperparam_dict_random_forest = {"n_estimators":[50,100,150,200],"criterion":['absolute_error', 'poisson', 'friedman_mse', 'squared_error'],"max_depth":[None,10,20],"max_features":["sqrt", "log2", None]}
     hyperparam_dict_grad_boost = {"loss":['huber', 'squared_error', 'absolute_error', 'quantile'],"learning_rate":[0.001,0.01,0.1],"n_estimators":[100,150,200]}
